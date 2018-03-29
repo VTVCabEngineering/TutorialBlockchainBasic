c87c7ef8b96bfe8a0844167a82593f49cb537b55
Kha Chuong <khachuong@vtvcab.vn>
2018-03-29 11:25:19 +0700

    ChuongNK: Add Part 2 Source code

---
+++ Part2/helloblockchain/app.js
@@ -0,0 +1,26 @@
+let Block = require('./models/block')
+let Transaction = require('./models/transaction')
+let BlockChain = require('./models/blockchain')
+
+//Tạo genesis block
+let genesisBlock = new Block()
+let blockchain = new BlockChain(genesisBlock)
+
+//Tạo ra giao dịch của block và thêm block vào blockchain
+let transaction = new Transaction('khachuong', 'quochung', 100)
+let firstBlock = blockchain.getNextBlock([transaction])
+blockchain.addBlock(firstBlock)
+
+let secondBlockTransaction = new Transaction('hongcuc', 'leanh', 150)
+let secondBlock = blockchain.getNextBlock([secondBlockTransaction])
+blockchain.addBlock(secondBlock)
+
+console.log(blockchain)
+
+console.log('[Before] Is this blockchain valid? ' + blockchain.isChainValid())
+
+let secondBlockTransactionModified = new Transaction('hongcuc', 'leanh', 100)
+blockchain.blocks[1].transactions = [secondBlockTransaction]
+
+console.log('[After] Is this blockchain valid? ' + blockchain.isChainValid())
+
---
+++ Part2/helloblockchain/models/blockchain.js
@@ -0,0 +1,62 @@
+let Block = require('./block')
+let sha256 = require('js-sha256')
+class BlockChain {
+    constructor(genesisBlock) {
+        this.blocks = []
+        this.addBlock(genesisBlock)
+    }
+
+    addBlock(block) {
+       if(this.blocks.length == 0) {
+            /* Nếu là block ban đầu (genesis block) thì thực hiện:
+              + Sinh ra mã băm mặc định cho thuộc tính previousHash = "0000000000000000"
+              + Sinh ra mã băm cho thuộc tính hash
+            */
+            block.previousHash = "0000000000000000"
+            block.hash = this.generateHash(block)
+            let currentTime = new Date();
+            block.timestamp = currentTime.toUTCString()
+       }
+       this.blocks.push(block)
+    }
+
+    getNextBlock(transactions) {
+        let block = new Block()
+        transactions.forEach( function(transaction){
+            block.addTransaction(transaction)
+        });
+
+        let previousBlock = this.getPreviousBlock() 
+        block.index = this.blocks.length
+        block.previousHash = previousBlock.hash
+        block.hash = this.generateHash(block)
+        let currentTime = new Date();
+        block.timestamp = currentTime.toUTCString()
+        return block
+    }
+
+    getPreviousBlock() {
+        return this.blocks[this.blocks.length-1]
+    }
+
+    generateHash(block) {
+        return sha256(block.key)
+    }
+
+    isChainValid() {
+        for(let i = 1; i < this.blocks.length; i++) {
+            const currentBlock = this.blocks[i]
+            const previousBlock = this.blocks[i-1]
+            if (currentBlock.hash !== this.generateHash(currentBlock)){
+                return false
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+module.exports = BlockChain
